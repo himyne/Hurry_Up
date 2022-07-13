2338b5006cdc16a268c60c5b1b8177dfa80b7d40
minhye <alsgp9898@naver.com>
2022-07-14 01:27:24 +0900

    🎉 Begin a project

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>허리업</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffc8c8',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#ff5e5e',
+    fontSize: 40, 
+  }
 });
+
